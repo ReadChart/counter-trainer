dff2f9713e89a782e5b1c7ea6b24a0ed2fe7c040
Richard Tung <zqdbyct@gmail.com>
2022-06-19 02:14:29 +0800

    feat: add counter w/o state

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Counter from './components/Counter'
 const rootEl = document.getElementById('root')
 
 const render = () => ReactDOM.render(
-    <Counter/>,
+    <Counter onIncrement={this.increase(this.props.value, 'INCREMENT')} onDecrement={this.increase(this.props.value, 'DECREMENT')} value={0}/>,
     rootEl
 )
 
